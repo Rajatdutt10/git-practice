d740361babc52d0aac01befea4fb356729adde62
Rajatdutt10 <rajatdutt5@gmail.com>
2024-03-12 15:58:07 -0400

    Starting Branching

--- src/App.java
+++ src/App.java
@@ -1,14 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int[] arr = {1,4,7,9,10,23,45};
-        int target = 10;
-        
-
-        Searching search = new Searching();
-
-
-        System.out.println(" target is fount at index: " + search.LinerSearch(arr, target));
-
-        System.out.println(" target is fount at index: " + search.sentinelSearch(arr, arr.length, target));
+        System.out.println("This is teh app.");
     }
 }
